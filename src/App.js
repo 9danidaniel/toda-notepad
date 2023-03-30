00f4b921e5bb4e41f0ff8365a0d49889fd7dbdfe
Daniel Azulay <danieldavidazulay@moonsite.co.il>
2023-03-30 11:24:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import logo from './assets/images/smiley.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [isTitleShown, setIsTitleShown] = useState(false);
+const [savedNote, setSavedNote] = useState(localStorage.getItem("note") ?? '');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header dir='rtl' className="App-header">
+        <div className='select-none fixed top-6 flex flex-row h-min items-center bg-[#b4530910] rounded-full  cursor-pointer'  onClick={() => setIsTitleShown(!isTitleShown)}>
+          <img src={logo} className="App-logo" alt="logo" />
+         {isTitleShown && <p className='text-7xl mr-1 text-amber-600 font-bold pl-6'>מכתב לתודה</p>}
+        </div>
+        <div className='mt-40 px-32 pb-20 h-[100000px]'>
+          <textarea 
+          value={savedNote}
+          onChange={(event) => {
+            const message = event.target.value;
+            localStorage.setItem("note", message);
+            setSavedNote(message);
+          }}
+          className='resize-none h-[100000px] overflow-wrap w-full font-varela  bg-transparent rounded-3xl outline-none text-amber-600 py-5 px-8 text-4xl leading-snug'
+           />
+        </div>
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'varela': 'Varela Round, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
